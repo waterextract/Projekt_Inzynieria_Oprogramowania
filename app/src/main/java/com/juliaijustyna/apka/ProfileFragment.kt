ce7d4e5006dfc71c505721eaed374416e385c79f
Y0zu <UL0266239@edu.uni.lodz.pl>
2024-04-22 03:59:45 +0200

    Initial Commit

--- app/src/main/java/com/juliaijustyna/apka/ProfileFragment.kt
+++ app/src/main/java/com/juliaijustyna/apka/ProfileFragment.kt
@@ -5,6 +5,12 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.TextView
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.database.DataSnapshot
+import com.google.firebase.database.DatabaseError
+import com.google.firebase.database.FirebaseDatabase
+import com.google.firebase.database.ValueEventListener
 
 // TODO: Rename parameter arguments, choose names that match
 // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
@@ -17,9 +23,9 @@ private const val ARG_PARAM2 = "param2"
  * create an instance of this fragment.
  */
 class ProfileFragment : Fragment() {
-    // TODO: Rename and change types of parameters
     private var param1: String? = null
     private var param2: String? = null
+    private lateinit var textViewUsername: TextView // Deklaracja textViewUsername
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -29,25 +35,42 @@ class ProfileFragment : Fragment() {
         }
     }
 
+    private fun loadUsername() {
+        val uid = FirebaseAuth.getInstance().currentUser?.uid
+        val userRef = uid?.let { FirebaseDatabase.getInstance().getReference("Users").child(it) }
+
+        if (userRef != null) {
+            userRef.addListenerForSingleValueEvent(object : ValueEventListener {
+                override fun onDataChange(dataSnapshot: DataSnapshot) {
+                    if (dataSnapshot.exists()) {
+                        val username = dataSnapshot.child("name").value.toString()
+                        // Przypisanie pobranej nazwy użytkownika do textViewUsername
+                        textViewUsername.text = username
+                    }
+                }
+
+                override fun onCancelled(databaseError: DatabaseError) {}
+            })
+        }
+    }
+
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?,
     ): View? {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_profile, container, false)
+        val view = inflater.inflate(R.layout.fragment_profile, container, false)
+
+        // Inicjalizacja textViewUsername
+        textViewUsername = view.findViewById(R.id.textViewUsername)
+
+        // Załaduj nazwę użytkownika
+        loadUsername()
+
+        return view
     }
 
     companion object {
-        /**
-         * Use this factory method to create a new instance of
-         * this fragment using the provided parameters.
-         *
-         * @param param1 Parameter 1.
-         * @param param2 Parameter 2.
-         * @return A new instance of fragment InfoFragment.
-         */
-        // TODO: Rename and change types and number of parameters
-        @JvmStatic
+
         fun newInstance(param1: String, param2: String) =
             ProfileFragment().apply {
                 arguments = Bundle().apply {
@@ -57,3 +80,12 @@ class ProfileFragment : Fragment() {
             }
     }
 }
+
+        fun newInstance(param1: String, param2: String) =
+            ProfileFragment().apply {
+                arguments = Bundle().apply {
+                    putString(ARG_PARAM1, param1)
+                    putString(ARG_PARAM2, param2)
+                }
+            }
+
--- app/src/main/java/com/juliaijustyna/apka/SignupActivity.kt
+++ app/src/main/java/com/juliaijustyna/apka/SignupActivity.kt
@@ -6,7 +6,10 @@ import android.os.Bundle
 import android.widget.Toast
 import com.juliaijustyna.apka.databinding.ActivitySignupBinding
 import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.database.DataSnapshot
+import com.google.firebase.database.DatabaseError
 import com.google.firebase.database.FirebaseDatabase
+import com.google.firebase.database.ValueEventListener
 
 
 class SignupActivity : AppCompatActivity() {
@@ -28,21 +31,42 @@ class SignupActivity : AppCompatActivity() {
             val username = binding.signupUsername.text.toString()
             val password = binding.signupPassword.text.toString()
             val confirmPassword = binding.signupConfirm.text.toString()
-            if (email.isNotEmpty() && password.isNotEmpty() && confirmPassword.isNotEmpty()){
-                if (password == confirmPassword){
-                    firebaseAuth.createUserWithEmailAndPassword(email, password).addOnCompleteListener(this){
-                        if (it.isSuccessful){
-                            updateUserInfo()
-                            val intent = Intent(this, LoginActivity::class.java)
-                            startActivity(intent)
+            if (email.isNotEmpty() && username.isNotEmpty() && password.isNotEmpty() && confirmPassword.isNotEmpty()){
+                // Sprawdź, czy nazwa użytkownika jest już w użyciu
+                val ref = FirebaseDatabase.getInstance().getReference("Users")
+                ref.orderByChild("name").equalTo(username).addListenerForSingleValueEvent(object : ValueEventListener {
+                    override fun onDataChange(dataSnapshot: DataSnapshot) {
+                        if (dataSnapshot.exists()) {
+                            // Nazwa użytkownika jest już zajęta, wyświetl komunikat
+                            Toast.makeText(this@SignupActivity, "Username is already taken", Toast.LENGTH_SHORT).show()
                         } else {
-                            Toast.makeText(this, it.exception.toString(), Toast.LENGTH_SHORT).show()
-                            finish()
+                            // Nazwa użytkownika jest dostępna, utwórz konto użytkownika
+                            if (password == confirmPassword){
+                                firebaseAuth.createUserWithEmailAndPassword(email, password).addOnCompleteListener(this@SignupActivity){
+                                    if (it.isSuccessful){
+                                        updateUserInfo()
+                                        val intent = Intent(this@SignupActivity, LoginActivity::class.java)
+                                        startActivity(intent)
+                                    } else {
+                                        Toast.makeText(this@SignupActivity, it.exception.toString(), Toast.LENGTH_SHORT).show()
+                                        finish()
+                                    }
+                                }
+                            } else {
+                                Toast.makeText(this@SignupActivity, "Password does not match", Toast.LENGTH_SHORT).show()
+                            }
                         }
                     }
-                } else {
-                    Toast.makeText(this, "Password does not matched", Toast.LENGTH_SHORT).show()
-                }
+
+                    override fun onCancelled(databaseError: DatabaseError) {
+                        // Obsłużanie błędu zapytania do bazy danych
+                        Toast.makeText(
+                            this@SignupActivity,
+                            "Database error: ${databaseError.message}",
+                            Toast.LENGTH_SHORT
+                        ).show()
+                    }
+                })
             } else {
                 Toast.makeText(this, "Fields cannot be empty", Toast.LENGTH_SHORT).show()
             }
