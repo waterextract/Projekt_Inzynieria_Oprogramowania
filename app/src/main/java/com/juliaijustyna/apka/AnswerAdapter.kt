4ca8f2d86bc614353284997a207876bece7ef92a
Y0zu <UL0266239@edu.uni.lodz.pl>
2024-05-12 20:32:43 +0200

    Initial Commit

--- app/src/main/java/com/juliaijustyna/apka/AnswerAdapter.kt
+++ app/src/main/java/com/juliaijustyna/apka/AnswerAdapter.kt
@@ -6,12 +6,13 @@ import android.widget.TextView
 import androidx.recyclerview.widget.RecyclerView
 
 data class Answer(val playerId: String, val answerText: String)
+//data class PlayerID(val playerId: String)
 
 
 class AnswersAdapter(private var answers: List<Answer>) : RecyclerView.Adapter<AnswersAdapter.AnswerViewHolder>() {
 
     inner class AnswerViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView) {
-        //val playerIdTextView: TextView = itemView.findViewById(R.id.playerIdTextView)
+        val playerIdTextView: TextView = itemView.findViewById(R.id.playerIdTextView)
         val answerTextView: TextView = itemView.findViewById(R.id.answerTextView)
     }
 
@@ -28,7 +29,7 @@ class AnswersAdapter(private var answers: List<Answer>) : RecyclerView.Adapter<A
 
     override fun onBindViewHolder(holder: AnswerViewHolder, position: Int) {
         val currentAnswer = answers[position]
-       // holder.playerIdTextView.text = currentAnswer.playerId
+       holder.playerIdTextView.text = currentAnswer.playerId
         holder.answerTextView.text = currentAnswer.answerText
     }
 
@@ -38,3 +39,34 @@ class AnswersAdapter(private var answers: List<Answer>) : RecyclerView.Adapter<A
 
 
 }
+
+/*
+class IDAdapter(private var ID: List<PlayerID>) : RecyclerView.Adapter<IDAdapter.IDViewHolder>() {
+
+    inner class IDViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView) {
+        val playerIdTextView: TextView = itemView.findViewById(R.id.playerIdTextView)
+       // val answerTextView: TextView = itemView.findViewById(R.id.answerTextView)
+    }
+
+    fun updateID(newID: List<PlayerID>) {
+        ID = newID.toMutableList()
+        notifyDataSetChanged()
+    }
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): IDViewHolder {
+        val itemView =
+            LayoutInflater.from(parent.context).inflate(R.layout.item_answer, parent, false)
+        return IDViewHolder(itemView)
+    }
+
+    override fun onBindViewHolder(holder: IDViewHolder, position: Int) {
+        val currentID = ID[position]
+        holder.playerIdTextView.text = currentID.playerId
+        //holder.answerTextView.text = currentAnswer.answerText
+    }
+
+    override fun getItemCount(): Int {
+        return ID.size
+    }
+}
+*/
