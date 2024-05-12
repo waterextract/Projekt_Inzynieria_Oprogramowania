8b3be774fb4e9a4318dcb1605b359debfbebc697
Ja <julka10101001@gmail.com>
2024-05-12 22:51:40 +0200

    Initial Commit

--- app/src/main/java/com/juliaijustyna/apka/AddPhotoActivity.kt
+++ app/src/main/java/com/juliaijustyna/apka/AddPhotoActivity.kt
@@ -1,53 +1,87 @@
 package com.juliaijustyna.apka
 
+import android.content.Intent
+import android.net.Uri
 import android.os.Bundle
+import android.provider.MediaStore
 import android.widget.Button
-import android.widget.EditText
 import android.widget.Toast
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
-import androidx.core.view.ViewCompat
-import androidx.core.view.WindowInsetsCompat
 import com.google.firebase.auth.FirebaseAuth
 import com.google.firebase.database.FirebaseDatabase
+import com.google.firebase.storage.FirebaseStorage
 
 class AddPhotoActivity : AppCompatActivity() {
 
-    private lateinit var submitButton : Button
-    private lateinit var database: FirebaseDatabase
+    private lateinit var submitButton: Button
+    private lateinit var storage: FirebaseStorage
+    private val PICK_IMAGE_REQUEST = 1
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContentView(R.layout.activity_add_photo)
         submitButton = findViewById(R.id.addPhoto)
-        database = FirebaseDatabase.getInstance()
+        storage = FirebaseStorage.getInstance()
 
         // Set onClickListener for the submit button
         submitButton.setOnClickListener {
-
-                // Save question to Firebase
-                savePhotoToFirebase()
-
+            openGallery()
         }
     }
 
-    private fun savePhotoToFirebase() {
+    private fun openGallery() {
+        val intent = Intent(Intent.ACTION_PICK, MediaStore.Images.Media.EXTERNAL_CONTENT_URI)
+        startActivityForResult(intent, PICK_IMAGE_REQUEST)
+    }
+
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+        super.onActivityResult(requestCode, resultCode, data)
+        if (requestCode == PICK_IMAGE_REQUEST && resultCode == RESULT_OK && data != null && data.data != null) {
+            val imageUri = data.data
+            if (imageUri != null) {
+                savePhotoToFirebase(imageUri)
+            }
+        }
+    }
+
+    private fun savePhotoToFirebase(imageUri: Uri) {
         val currentUser = FirebaseAuth.getInstance().currentUser
         val uid = currentUser?.uid
 
         if (uid != null) {
-            // Generowanie unikalnego 3-liczbowego identyfikatora
+            val storageRef = storage.reference
             val questionId = generateQuestionId()
+            val filePath = storageRef.child("images").child("$questionId.jpg")
+
+            filePath.putFile(imageUri)
+                .addOnSuccessListener {
+                    filePath.downloadUrl.addOnSuccessListener { uri ->
+                        val imageUrl = uri.toString()
+                        val database = FirebaseDatabase.getInstance()
+                        val questionsRef = database.reference.child("photos").child(questionId)
+                        questionsRef.setValue(imageUrl)
+                            .addOnSuccessListener {
+                                Toast.makeText(this, "Przesłano pomyślnie", Toast.LENGTH_SHORT).show()
+                            }
+                            .addOnFailureListener {
+                                Toast.makeText(this, "Błąd przesyłania: ${it.message}", Toast.LENGTH_SHORT).show()
+                            }
+                    }
+                }
+                .addOnFailureListener {
+                    Toast.makeText(this, "Błąd przesyłania: ${it.message}", Toast.LENGTH_SHORT).show()
+                }
         }
     }
 
     private fun generateQuestionId(): String {
         val random = java.util.Random()
         val questionId = StringBuilder()
-        for (i in 0 until 3) {
+        for (i in 0 until 4) {
             questionId.append(random.nextInt(10))
         }
         return questionId.toString()
     }
-
 }
